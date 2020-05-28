4e5b11d904aed4fbd471a395d5050ed2f6063ba0
yummy-qiz <gigi.zhang.opt@gmail.com>
2020-05-28 17:08:49 +0800

    second commit

--- correct.py
+++ correct.py
@@ -13,7 +13,7 @@ import re
 import numpy as np
 
 
-TRAIN_PATH = '/home/qiz/correct_typos/data/'
+TRAIN_PATH = '/home/qiz/correct_typos/data1/'
 PRIOR_WEIGHT = 1.25
 #CHARACTER_DISTANCE_LIMIT = 1
 WORD_DISTANCE_LIMIT = 4
@@ -23,7 +23,7 @@ Latin = [' ', '\"', '#', '$', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
          'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e',
          'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}']
 
-def abnormal_correction(LM, text, unigrams_file, bigrams_file, vocabulary_file):
+def abnormal_correction(LM, text):
     tokens = re.split(" ", text)
     for i, t in enumerate(tokens):
         for j in range(len(t)):
@@ -31,43 +31,64 @@ def abnormal_correction(LM, text, unigrams_file, bigrams_file, vocabulary_file):
                 continue
             else:
                 #找到所有可能单词，取前三个，然后排除掉cocabulary中不存在的，再根据上下文判断，或者贝叶斯判断。
-                #没有考虑短单词的情况
                 typos1 = t[j-1:j+1]
                 typos2 = t[j:j+2]
-                Dic = edits(LM, typos1, typos2, unigrams_file, bigrams_file, vocabulary_file)
+                Dic = edits(typos1, typos2)
+                #Dic = sorted(Dic.items(), key = lambda items:items[1], reverse=True)
                 Dic = sorted(Dic.items(), key = lambda items:items[1])
-                print(Dic)
+                #取可能性最大的前五
+                Dic = Dic[:5]
+                can_word = []
+                for m in range(len(Dic)):
+                    t = t.replace(t[j], Dic[m][0])
+                    can_word.append(t)
+                vocabulary = getVocabulary()
+                #删掉不存在词库中的词汇
+                for word_c in can_word:
+                    if word_c not in vocabulary:
+                        can_word.remove(word_c)
 
 
+def getVocabulary():
+    vocabulary = []
+    for line in open(os.path.join(Path(TRAIN_PATH, "vocabulary.count")), "r", encoding="utf8"):
+        vocList = line.split('\t')
+        vocabulary.append(vocList)
+    return vocabulary
+"""
+def get_max_prob(can_word, vocabulary):
+    for i in range(len(can_word)):
+        if can_word[i] in vocabulary:
+            pass
+"""
 
-def edits(LM, typos1, typos2, unigrams_file, bigrams_file, vocabulary_file):
-    uni = open(Path(unigrams_file), "r")
-    bi = open(Path(bigrams_file), "r")
-    vocabualry = open(Path(vocabulary_file), "r")
+def edits(typos1, typos2):
+    bi = open(os.path.join(Path(TRAIN_PATH, "bigrams.count")), "r")
     dic = {}
     for line in bi:
         line_ = re.split('\t', line)
         if typos1[0] == line_[0][0]:
             can1 = line_[0]
             prob1 = float(line_[2])
-            bi_ = open(Path(bigrams_file), "r")
+            bi_ = open(os.path.join(Path(TRAIN_PATH, "bigrams.count")), "r")
             for line1 in bi_:
-                line_1 = re.split('\t', line1)
+                line_1 = line1.split('\t')
                 if can1[1] == line_1[0][0] and typos2[1] == line_1[0][1]:
-                    can2 = line_1[0]
+                    #can2 = line_1[0]
                     prob2 = float(line_1[2])
+                    uni = open(os.path.join(Path(TRAIN_PATH, "unigrams.count")), "r")
                     for line2 in uni:
-                        line_2 = re.split('\t', line2)
+                        line_2 = line2.split('\t')
                         if can1[1] ==line_2[0]:
                             prob3 = float(line_2[2])
                             pro = (prob1 + prob2)*prob3
                             dic_1 = {can1[1]: pro}
                             dic.update(dic_1)
+                    #pro = prob1 + prob2
+                    #dic_1 = {can1[1]:pro}
+                    #dic.update(dic_1)
     return dic
-
-
-
 
 if __name__ == "__main__":
     LM = open(Path(TRAIN_PATH + 'LM2.arpa'))
-    abnormal_correction(LM, "b¡tcoin address", "./data/unigrams.count", './data/bigrams.count', './data/vocabulary.count')
+    abnormal_correction(LM, "th¡ng")
